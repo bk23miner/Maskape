c9bd8f367f99848ea592f06d18e3df3452389594
bk23miner <bk23miner@gmail.com>
2026-01-30 22:09:34 +0100

    stuff

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,1 +1,39 @@
-extends Node
+extends CharacterBody2D
+
+
+
+
+
+
+	
+	
+	
+
+
+@export var SPEED := 200.0
+@export var DASH := 300.0
+var dashing = false
+var cooldown = true
+
+func _physics_process(delta):
+		var input_direction = Input.get_vector("left", "right", "up", "down")
+		if Input.is_action_just_pressed("dash") and cooldown:
+			dashing = true
+			cooldown = false
+			$dash_timer.start()
+			$cooldown.start()
+		if dashing:
+			velocity = input_direction * (SPEED + DASH)
+		else:
+			velocity = input_direction * SPEED
+		move_and_slide()
+
+
+
+func finish_dash():
+	dashing = false
+	print("Test")
+
+
+func finish_cooldown():
+	cooldown = true
