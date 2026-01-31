fdb3c6910c830b8cc6b729a070fee2473c767a8e
chxnxilxe <chanmi.lee0811@gmail.com>
2026-01-31 15:06:06 +0100

    Color randomization

--- scripts/mask_piece.gd
+++ scripts/mask_piece.gd
@@ -6,12 +6,13 @@ var collected_by_gate = false
 var player
 var gate
 
+@onready var sprite: Sprite2D = $Sprite2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
 	gate = get_tree().get_first_node_in_group("gate")
-	pass # Replace with function body.
-
+	sprite.modulate = Color.from_hsv(randf(), 1, 1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
