0349572c48423bab295b51bf561527fb3f0c762d
bk23miner <bk23miner@gmail.com>
2026-02-01 11:20:19 +0100

    world_0 added

--- scripts/lantern.gd
+++ scripts/lantern.gd
@@ -1,20 +1,20 @@
 extends Node2D
-
-@export var texture = 0
+@onready var lantern_img: Sprite2D = $lantern_img
+@export var lantern_texture = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if texture == 0:
-		$Sprite2D.texture = load("res://assets/stuff/lanterns/purple-lantern.png")
-	elif texture == 1:
-		$Sprite2D.texture = load("res://assets/stuff/lanterns/blue-lantern.png")
-	elif texture == 2:
-		$Sprite2D.texture = load("res://assets/stuff/lanterns/green-lantern.png")
-	elif texture == 3:
-		$Sprite2D.texture = load("res://assets/stuff/lanterns/red-lantern.png")
-	elif texture == 4:
-		$Sprite2D.texture = load("res://assets/stuff/lanterns/pink-lantern.png")
-	elif texture == 5:
-		$Sprite2D.texture = load("res://assets/stuff/lanterns/yellow-lantern.png")
+	if lantern_texture == 0:
+		lantern_img.texture = load("res://assets/stuff/lanterns/purple-lantern.png")
+	elif lantern_texture == 1:
+		lantern_img.texture = load("res://assets/stuff/lanterns/blue-lantern.png")
+	elif lantern_texture == 2:
+		lantern_img.texture = load("res://assets/stuff/lanterns/green-lantern.png")
+	elif lantern_texture == 3:
+		lantern_img.texture = load("res://assets/stuff/lanterns/red-lantern.png")
+	elif lantern_texture == 4:
+		lantern_img.texture = load("res://assets/stuff/lanterns/pink-lantern.png")
+	elif lantern_texture == 5:
+		lantern_img.texture = load("res://assets/stuff/lanterns/yellow-lantern.png")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
